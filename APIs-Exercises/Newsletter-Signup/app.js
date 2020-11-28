d4d2872cfac0d5acf44feed3aee56a05039a61b5
QiluHuang <lunahuang@foxmail.com>
2020-11-28 02:15:06 -0800

    front-end

--- APIs-Exercises/Newsletter-Signup/app.js
+++ APIs-Exercises/Newsletter-Signup/app.js
@@ -8,7 +8,7 @@ app.use(express.static("public"));  // 处理本地static css和图片
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/signup.html");
 }) 
 
 app.post("/", function(req, res) {
