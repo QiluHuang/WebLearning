49cc077d0f61a345c88328daa3f3dfab1d2f7b63
QiluHuang <lunahuang@foxmail.com>
2020-12-31 04:52:58 -0800

    keeper-Dec30

--- React/Projects/keeper/src/views/App/App.jsx
+++ React/Projects/keeper/src/views/App/App.jsx
@@ -3,15 +3,27 @@ import React from "react";
 import Header from "../../components/Header/Header.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
 import Note from "../../components/Note/Note.jsx";
+import notes from "../../notes.js";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Note />
-      <Footer />
-    </div>
-  );
-}
+
+function createNotes(noteItem) {
+    return (
+      <Note
+        key={noteItem.key}
+        title={noteItem.title}
+        content={noteItem.content}
+      />
+    );
+  }
+  
+  function App() {
+    return (
+      <div>
+        <Header />
+        {notes.map(createNotes)}
+        <Footer />
+      </div>
+    );
+  }
 
 export default App;
