7845a3350c24111a1528a9a61e3e077ef1a890e5
QiluHuang <lunahuang@foxmail.com>
2020-12-31 04:35:10 -0800

    React-Dec30

---
+++ React/Projects/keeper/src/views/App/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "../../components/Header/Header.jsx";
+import Footer from "../../components/Footer/Footer.jsx";
+import Note from "../../components/Note/Note.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
