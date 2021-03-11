feae07f9ed6fd77697c8c8d5cb67003178e37b1e
QiluHuang <lunahuang@foxmail.com>
2021-03-11 13:11:08 -0800

    get and post

---
+++ RESTful-APIs/getpostExer/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.set('views', path.join(__dirname, 'views'));
+app.use('view engine', 'ejs');
+
+
+const comments = [
+    {
+        username: 'Todd',
+        comment: 'lol that is so funny!'
+    },
+    {
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account, Todd'
+    },
+    {
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    }
+]
+
+app.get('comments', (req, res) => {
+    res.render('comments/index');
+})
+
+
+app.get('/tacos', (req, res) => {
+    res.send("GET /tacos response");
+})
+
+app.post('/tacos', (req, res) => {
+    const { meat, qty } = req.body;
+    res.send(`OK, here are your ${qty} ${meat}`);
+})
+
+app.listen(PORT, () => {
+    console.log(`ON PORT ${PORT}`);
+})
